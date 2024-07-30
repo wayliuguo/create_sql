71fa9956d403841b026fe78814d74d179404af74
guoweiwliu <guoweiwliu@lexin.com>
2024-07-30 21:00:43 +0800

    feat: 支持实时维度sql生成

--- libs/generate_sql.js
+++ libs/generate_sql.js
@@ -17,11 +17,37 @@ function generateSql(config, reportList) {
     }
 
     if (config.realtimes_sql) {
-        // generateRealtimesSql(config, reportList)
+        const sqlQuery = generateRealtimesSql(reportList)
+        createSqlText('realtimes_sql', sqlQuery)
     }
 }
 
-// 生成sql字符串
+// 生成实时维度sql字符串
+function generateRealtimesSql(reportList) {
+    const sqlParts = ['SELECT', '  toStartOfMinute(toDateTime(fdeal_time - (fdeal_time % 600))) AS formatted_datetime,']
+
+    reportList.forEach(pos => {
+        const list = [
+            `  COUNT(DISTINCT CASE WHEN (fevent_pos = '${pos.hottag}' AND fdate = (toDate(now())) - INTERVAL 1 DAY) THEN fuid END) AS \`昨日${pos.eventPosName}\`,`,
+            `  COUNT(DISTINCT CASE WHEN (fevent_pos = '${pos.hottag}' AND fdate = (toDate(now()))) THEN fuid END) AS \`今日${pos.eventPosName}\`,`,
+            `  COUNT(DISTINCT CASE WHEN (fevent_pos = '${pos.hottag}' AND fdate = (toDate(now())) - INTERVAL 1 DAY) THEN fuid END) AS \`近7日${pos.eventPosName}\`,`
+        ]
+        sqlParts.push(...list)
+    })
+
+    // 移除最后一个逗号
+    sqlParts[sqlParts.length - 1] = sqlParts[sqlParts.length - 1].slice(0, -1)
+
+    sqlParts.push(`FROM dp_click.t_user_behavior_business_log`)
+    sqlParts.push("WHERE fdate BETWEEN '${startDateFql}'\n  AND '${endDateFql}'")
+    sqlParts.push(`  AND fevent_pos in (${reportList.map(pos => `'${pos.hottag}'`).join(', ')})`)
+    sqlParts.push(`GROUP BY formatted_datetime`)
+    sqlParts.push(`ORDER BY formatted_datetime DESC`)
+
+    return sqlParts.join('\n')
+}
+
+// 生成天维度sql字符串
 function generateDaysSql(reportList) {
     const sqlParts = ['SELECT', '  fdate,']
 
@@ -42,6 +68,7 @@ function generateDaysSql(reportList) {
     return sqlParts.join('\n')
 }
 
+// 生成文件
 function createSqlText(name, sqlStr) {
     fs.writeFile(`${name}.txt`, sqlStr, err => {
         if (err) {
